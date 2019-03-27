95e5692971c90c3a31dacfdaba9f52c75be8efef
ManotLuijiu <manot_luijiu@hotmail.com>
2019-03-27 19:09:08 +0700

    test cache

---
+++ src/components/Home/info.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+export default function info() {
+  return (
+    <section className="py-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-10 col-sm-8 mx-auto text-center">
+            <p className="lead text-muted mb-5">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui
+              optio nam beatae culpa error? Tempora a id cumque, temporibus
+              fugiat et dolore praesentium voluptas ea voluptatum consequatur
+              quae velit quia reiciendis culpa ipsam quam laborum vel aut natus
+              optio! Iure expedita in adipisci delectus laboriosam nobis
+              voluptate, nesciunt reprehenderit, nisi reiciendis culpa dolorem
+              explicabo modi, dolores eius voluptatibus tempora dignissimos
+              illum temporibus. Et, neque? Facere, earum dolorum. Tenetur
+              molestiae accusamus laboriosam quam, id quod eaque commodi vero
+              incidunt cupiditate dignissimos quasi error itaque voluptatibus,
+              veritatis est architecto cum ut minima porro! Officia dolor quasi
+              accusamus deleniti eligendi nisi perferendis quisquam?
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql } from 'gatsby';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
-import { FaGulp } from 'react-icons/fa';
+import BackgroundSection from '../components/Globals/BackgroundSection';
+import Info from '../components/Home/info';
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO
       title="Home"
@@ -19,10 +20,24 @@ const IndexPage = () => (
         `taxconsulting`,
       ]}
     />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <FaGulp />
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="moocoding's"
+    />
+    <Info />
   </Layout>
 );
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "default-background.jpeg" }) {
+      childImageSharp {
+        fluid(quality: 100) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`;
 
 export default IndexPage;
