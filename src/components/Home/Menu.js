b84a82b00b3b5445e0d2b9b12cf1da2be01a4455
ManotLuijiu <manot_luijiu@hotmail.com>
2019-03-29 16:16:51 +0700

    adding product.js and products.js

--- src/components/Home/Menu.js
+++ src/components/Home/Menu.js
@@ -3,7 +3,13 @@ import Title from '../Globals/Title';
 import Img from 'gatsby-image';
 
 const getCategories = items => {
-  return items;
+  let tempItems = items.map(items => {
+    return items.node.category;
+  });
+  let tempCategories = new Set(tempItems);
+  let categories = Array.from(tempCategories);
+  categories = ['all', ...categories];
+  return categories;
 };
 
 export default class Menu extends Component {
@@ -15,15 +21,45 @@ export default class Menu extends Component {
       categories: getCategories(props.items.edges),
     };
   }
+  handleItems = category => {
+    let tempItems = [...this.state.items];
+    if (category === 'all') {
+      this.setState(() => {
+        return { coffeeItems: tempItems };
+      });
+    } else {
+      let items = tempItems.filter(({ node }) => node.category === category);
+      this.setState(() => {
+        return { coffeeItems: items };
+      });
+    }
+  };
 
   render() {
-    console.log(this.state.categories);
     if (this.state.items.length > 0) {
       return (
         <section className="menu py-5">
           <div className="container">
             <Title title="best of our menu" />
             {/* categories */}
+            <div className="row mb-5">
+              <div className="col-10 mx-auto text-center">
+                {this.state.categories.map((category, index) => {
+                  return (
+                    <button
+                      type="button"
+                      key={index}
+                      className="btn btn-yellow text-capitalize m-3"
+                      onClick={() => {
+                        this.handleItems(category);
+                      }}
+                    >
+                      {category}
+                    </button>
+                  );
+                })}
+              </div>
+            </div>
             {/* items */}
             <div className="row">
               {this.state.coffeeItems.map(({ node }) => {
